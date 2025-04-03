2b6e8c5d2953b256496cab30a7378bc8dc870098
Сослан Гутнов <soslan.gutnov157@gmail.com>
2025-04-03 23:12:45 +0300

    Добавил представления для авторизации и регистрации

---
+++ Shop/Controllers/AuthorizationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
