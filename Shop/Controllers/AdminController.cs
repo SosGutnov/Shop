5cdd57b83fcbbb91bd7454bfc0fb34b041e044c2
Сослан Гутнов <soslan.gutnov157@gmail.com>
2025-04-04 22:02:09 +0300

    Добавил панель администратора

---
+++ Shop/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View("Orders");
+        }
+        public IActionResult Products()
+        {
+            return View("Products");
+        }
+        public IActionResult Roles()
+        {
+            return View("Roles");
+        }
+        public IActionResult Users()
+        {
+            return View("Users");
+        }
+    }
+}
